65d9fe63bfd5573040019e6768b1386cea9c3157
relentless-coder <contact@ayushbahuguna.com>
2018-05-15 13:35:49 +0530

    directly return files

--- get_files.py
+++ get_files.py
@@ -1,21 +1,24 @@
+"""Module to fetch files from path"""
 import os
-import json
 from request_handler import request_handler
 from error import ResponseError
 
 
 def get_files():
+    """Returns JSON if success, else raises error"""
     files = []
     if files:
         return files
     url = 'https://api.github.com/repos/{:1}/{:2}/contents/{:3}'\
-          .format(os.environ('GITHUB_USER'), os.environ('GITHUB_REPO'),
-                  os.environ('CONTENT_PATH'))
+          .format(os.environ['GITHUB_USER'], os.environ['GITHUB_REPO'],
+                  os.environ['CONTENT_PATH'])
     headers = {'Authorization': 'token {:1}'
-               .format(os.environ('GITHUB_TOKEN'))}
-    r = request_handler('get', url, headers=headers)
-    if r.status_code != 200:
-        raise ResponseError(r.status_code, json.loads(r.json()['message']))
+                                .format(os.environ['GITHUB_TOKEN']),
+               'User-Agent': 'my_api'
+              }
+    received_request = request_handler('get', url, headers=headers)
+    if received_request.status_code != 200:
+        raise ResponseError(received_request.status_code,
+                            received_request.json()['message'])
     else:
-        files = json.loads(r.json())
-        return files
+        return received_request.json()
