e1139bd1b93c24cf32a9c4bc86aeee6b6378b7d5
relentless-coder <contact@ayushbahuguna.com>
2018-05-15 13:36:59 +0530

    if CONTENT_PATH is present then adjust filename accordingly

--- parse_event.py
+++ parse_event.py
@@ -1,16 +1,22 @@
+"""Parses event"""
 import json
 import os
 
 
 def parse_event(event):
-    attrs = event.Records[0].Sns.MessageAttributes
+    """Return the head commit id
+    Arguments:
+    :event - the event object that contains the commit info
+    """
+    attrs = event.get('Records')[0].get('Sns').get('MessageAttributes')
+
     try:
-        if attrs.get('X-Github-Event') and attrs.get('X-Github-Event') == 'push':
-            eventObj = json.loads(event.Records[0].Sns.Message)
-            foundBranch = eventObj.ref.split('/')[-1]
-            if foundBranch == os.environ('branch'):
-                return eventObj['head_commit']['id']
-            else:
-                return False
+        if attrs.get('X-Github-Event') and attrs.get('X-Github-Event').get('Value') == 'push':
+            event_obj = json.loads(event.get('Records')[0].get('Sns').get('Message'))
+            found_branch = event_obj['ref'].split('/')[-1]
+            if found_branch == os.environ['branch']:
+                return event_obj['head_commit']['id']
+            return False
+        return False
     except AttributeError:
         raise
