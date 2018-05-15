8741a671b933adc0ff304ca323fea010a0903783
relentless-coder <contact@ayushbahuguna.com>
2018-05-15 13:34:50 +0530

    no need to load json

--- get_blob.py
+++ get_blob.py
@@ -1,21 +1,23 @@
+"""Fetches the git blob"""
 import os
-import json
 from request_handler import request_handler
 from error import ResponseError
 
 
-def get_blob(fileSha):
+def get_blob(sha):
     """Returns the base64 content for a file
     :param fileSha: sha of the file
     """
     url = 'https://api.github.com/repos/{:1}/{:2}/git/blobs/{:3}'\
-          .format(os.environ('GITHUB_USER'),
-                  os.environ('GITHUB_REPO'), fileSha)
+          .format(os.environ['GITHUB_USER'],
+                  os.environ['GITHUB_REPO'], sha)
     headers = {'Authorization': 'token {:1}'
-               .format(os.environ('GITHUB_TOKEN'))}
-    r = request_handler('get', url, headers=headers)
-    if r.status_code != 200:
-        raise ResponseError(r.status_code,
-                            json.loads(r.json())['message'])
+                                .format(os.environ['GITHUB_TOKEN']),
+               'User-Agent': 'my_api'
+              }
+    received_request = request_handler('get', url, headers=headers)
+    if received_request.status_code != 200:
+        raise ResponseError(received_request.status_code,
+                            received_request.json()['message'])
     else:
-        return json.loads(r.json())['content']
+        return received_request.json()['content']
