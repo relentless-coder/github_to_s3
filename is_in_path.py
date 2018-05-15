4ba61f811f44d15eae53f9a7347c3d69b2969de4
relentless-coder <contact@ayushbahuguna.com>
2018-05-15 13:36:09 +0530

    follow naming convention

--- is_in_path.py
+++ is_in_path.py
@@ -1,9 +1,14 @@
+"""Checks whether as file is in path provided"""
 from get_files import get_files
 
 
 def is_in_path(sha):
+    """Returns boolean
+    Arguments:
+        sha - the sha of the file
+    """
     files = get_files()
-    for f in files:
-        if f.get('sha') == sha:
+    for local_file in files:
+        if local_file.get('sha') == sha:
             return True
     return False
